d07b438c8f522699782f7b9a5ff1e9bdb897f39f
Yufeng Xie <yxie54@ucsc.edu>
2021-10-13 12:32:04 +0800

    Add Red Enemies and Katana into the game

--- docs/DSamurai/main.js
+++ docs/DSamurai/main.js
@@ -22,21 +22,45 @@ ll l l
 llllll
 ll  ll
 `,
+    `
+llllll
+ll l l
+ll l l
+llllll
+ l  l
+ l  l
+`,
+    `
+llllll
+ll l l
+ll l l
+llllll
+ll  ll
+`,
 ];
 
 // Game runtime options
 // Refer to the official documentation for all available options
 options = {};
 
+const GAME = {
+    HITBOX_OFFSET: 6
+}
+
+// Declare types of variables
+/** @type {{x: number, vx: number, activeState: boolean}} */
+let player;
 // Declare types of variables
 /** @type {{x: number, vx: number}} */
-let player;
+let enemy;
+let hitBoxDirection;
 
 // The game loop function
 function update() {
     // The init function
     if (!ticks) {
-        player = { x: 40, vx: 1 };
+        player = { x: 40, vx: 1, activeState: false };
+        enemy = { x: 10, vx: 1 };
     }
 
     // Turn player moving direction when [Tab] or reach left & right bounds
@@ -49,15 +73,39 @@ function update() {
         player.vx *= -1;
     }
     player.x += player.vx * sqrt(difficulty);
+    color("green");
+
+    if(player.vx > 0){
+        hitBoxDirection = player.x + GAME.HITBOX_OFFSET/2.25;
+    } else {
+        hitBoxDirection = player.x - GAME.HITBOX_OFFSET;
+    }
+    rect(hitBoxDirection, 80, 3, 8);
+
+    // enemy.x.distanceTo()
+
+    if (player.x < enemy.x) {
+        enemy.vx = -1;
+    } else {
+        enemy.vx = 1;
+    };
+    enemy.x += enemy.vx * sqrt(difficulty);
+
+
+    if (char(addWithCharCode("c", floor(ticks / 10) % 2), enemy.x, 87, {
+        mirror: { x: player.vx > 0 ? 1 : -1},}).isColliding.rect){
+            
+        }
 
     // Draw player in the scene, give it animation and set collider for end condition
     color("black");
     if (
         char(addWithCharCode("a", floor(ticks / 10) % 2), player.x, 87, {
             mirror: { x: player.vx > 0 ? 1 : -1 },
-        }).isColliding.rect.yellow
+        }).isColliding.char.c
     ) {
         play("lucky");
-        end();
+        // end();
     }
+    
 }
