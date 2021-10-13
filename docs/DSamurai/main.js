20063fda4027fcc8a5b5de441180fc9bc49ac763
Yufeng Xie <yxie54@ucsc.edu>
2021-10-13 10:07:28 +0800

    Initial Doc Set Up

---
+++ docs/DSamurai/main.js
@@ -0,0 +1,21 @@
+// The title of the game to be displayed on the title screen
+title  =  "D Samurai";
+
+// The description, which is also displayed on the title screen
+description  =  ` [Tab] Move\n\n[Hold] Deflect
+`;
+
+// The array of custom sprites
+characters  = [];
+
+// Game runtime options
+// Refer to the official documentation for all available options
+options  = {};
+
+// The game loop function
+function  update() {
+    // The init function
+    if (!ticks) {
+        
+    }
+}
