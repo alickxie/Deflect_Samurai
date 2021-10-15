40acb923a6ef45d99dc4e23bd13a2845fb728ae8
Yufeng Xie <yxie54@ucsc.edu>
2021-10-15 14:57:38 +0800

    Prototype Finished

--- docs/DSamurai/main.js
+++ docs/DSamurai/main.js
@@ -77,7 +77,13 @@ const GAME = {
 
 // Game runtime options
 // Refer to the official documentation for all available options
-options = { viewSize: { x: GAME.WIDTH, y: GAME.HEIGHT } };
+options = {
+    viewSize: { x: GAME.WIDTH, y: GAME.HEIGHT },
+    isPlayingBgm: true,
+    theme: "crt",
+    seed: 80,
+    isReplayEnabled: true
+};
 
 // Declare types of variables
 /** @type {{pos: Vector, vx: number, activeState: boolean}} */
@@ -96,8 +102,11 @@ let nextEnemyTicks;
  */
 let rangeEnemy;
 let nextRangeEnemyTicks;
-/** @type {{pos: Vector, vel: Vector}[]} */
+/** @type {{pos: Vector, vel: Vector, isDeflected: Boolean}[]} */
 let bullets;
+/** @type {{pos: Vector, radius: number, ticks: number, duration: number}[]} */
+let explosions;
+let multiplier;
 
 
 // The game loop function
@@ -108,6 +117,7 @@ function update() {
         enemy = [];
         rangeEnemy = [];
         bullets = [];
+        explosions = [];
         Deflecting = new Boolean(false);
     }
 
@@ -115,7 +125,7 @@ function update() {
     color("black");
     rect(0, GAME.Y_OFFSET + 3, GAME.WIDTH, GAME.Y_OFFSET);
     // Terrain (Upper Level)
-    color("red");
+    color("yellow");
     rect(0, 40, GAME.WIDTH, 2);
 
     // Push for Red enemy array information
@@ -138,7 +148,6 @@ function update() {
         });
         nextEnemyTicks = rnd(120, 150) / sqrt(difficulty);
     }
-    text(enemy.length.toString(), 15, 10);
 
     // Push Range Enemy Into the Field
     if (rangeEnemy.length === 0) {
@@ -162,15 +171,21 @@ function update() {
             vx,
             angle: -PI / 2,
             angleVel: av * 0.02,
-            speed: (0.1 / sqrt(av)) * sqrt(difficulty),
+            speed: (0.5 / sqrt(av)) * sqrt(difficulty),
             fireTicks: fireInterval,
             fireInterval,
             fireSpeed: rnd(1, 1.5) * sqrt(difficulty),
         });
-        nextRangeEnemyTicks = rnd(60, 80) / sqrt(difficulty);
+        nextRangeEnemyTicks = rnd(150, 200) / sqrt(difficulty);
     }
-    color("yellow");
-    text(rangeEnemy.length.toString(), 30, 10);
+
+    // Player is dflecting or not
+    if (input.isPressed) {
+        Deflecting = true;
+    }
+    if (input.isJustReleased) {
+        Deflecting = false;
+    }
 
     // Turn player moving direction when [Tab] or reach left & right bounds
     if (
@@ -181,15 +196,12 @@ function update() {
         // play("laser");
         player.vx *= -1;
     }
-    player.pos.x += (player.vx * sqrt(difficulty)) / GAME.PLAYER_SPEED;
+    if (!Deflecting) {
+        player.pos.x += (player.vx * sqrt(difficulty)) / GAME.PLAYER_SPEED;
+    } else {
+        player.pos.x += 0;
+    }
 
-    // Player is dflecting or not
-    if (input.isPressed) {
-        Deflecting = true;
-    }
-    if (input.isJustReleased) {
-        Deflecting = false;
-    }
     // Offset the sword into the right place.
     if (player.vx > 0) {
         hitBoxDirection = player.pos.x + GAME.HITBOX_OFFSET;
@@ -209,7 +221,7 @@ function update() {
     if (Deflecting) {
         color("transparent");
     } else {
-        color("light_blue");
+        color("black");
     }
     rect(hitBoxDirection - 1, 75, 2, 11);
 
@@ -221,7 +233,7 @@ function update() {
     }
     char("f", hitBoxDirection, 82, { mirror: { x: player.vx > 0 ? -1 : 1 } });
     if (Deflecting) {
-        color("light_blue");
+        color("black");
     } else {
         color("transparent");
     }
@@ -229,21 +241,32 @@ function update() {
 
     // Enemy Conditions
     remove(enemy, (e) => {
-        e.x += e.vx * sqrt(difficulty);
-        color('red');
+        e.x += (e.vx * sqrt(difficulty)) / 2;
 
+        color('red');
         if (char(addWithCharCode("c", floor(ticks / 10) % 2), e.x, GAME.Y_OFFSET).isColliding.char.e) {
             addScore(10 * difficulty, vec(e.x, GAME.Y_OFFSET));
             // addScore(multiplier, t.pos);
-            play('coin');
+            play("hit");
+            particle(e.x, GAME.Y_OFFSET);
             return (true);
         }
+    });
+    color("light_red");
+    if (explosions.length === 0) {
+        multiplier = 1;
+    }
+    remove(explosions, (e) => {
+        e.ticks--;
+        const r = e.radius * sin((e.ticks / e.duration) * PI);
+        arc(e.pos, r);
+        return e.ticks < 0;
     });
 
     color("yellow");
     // Range Enemy Conditions
     remove(rangeEnemy, (r) => {
-        r.pos.x += r.vx * r.speed;
+        r.pos.x += r.vx * (r.speed / 2);
         const ta = r.pos.angleTo(player.pos);
         if (abs(ta) < r.angleVel) {
             r.angle = ta;
@@ -253,80 +276,73 @@ function update() {
             r.angle += r.angleVel;
         }
         bar(r.pos, 3, 2, r.angle, -0.5);
-        // if (
-        //     char(addWithCharCode("c", floor(ticks / 25) % 2), r.pos, {
-        //         mirror: { x: r.vx < 0 ? -1 : 1 },
-        //     }).isColliding.rect.light_red
-        // ) {
-        //     play("powerUp");
-        //     addScore(multiplier, r.pos);
-        //     multiplier++;
-        //     particle(r.pos);
-        //     return true;
-        // }
+
+        if (
+            char(addWithCharCode("g", floor(ticks / 25) % 2), r.pos, {
+                mirror: { x: r.vx < 0 ? -1 : 1 },
+            }).isColliding.rect.light_red
+        ) {
+            play("hit");
+            addScore(10, r.pos);
+            particle(r.pos);
+            return true;
+        }
+
         r.fireTicks--;
         if (r.fireTicks < 0) {
             play("laser");
             bullets.push({
                 pos: vec(r.pos),
                 vel: vec(r.fireSpeed, 0).rotate(r.angle),
+                isDeflected: false
             });
             r.fireTicks = r.fireInterval;
         }
-        // return r.pos.x < -3 || r.pos.x > 103;
+        return r.pos.x < -91 || r.pos.x > 201;
     });
 
-
-    // Bullet Conditions
-    remove(bullets, (b) => {
-        b.pos.add(b.vel);
-        // color(b.vel.y < 0 ? "red" : "purple");
-        const c = bar(b.pos, 4, 3, b.vel.angle).isColliding;
-        // if (b.vel.y < 0) {
-        //     if (c.rect.blue) {
-        //         play("coin");
-        //         particle(b.pos, 9, 1 + ufo.power * 2, ufo.angle + PI / 2, PI / 8);
-        //         const ra = b.vel.angle - ufo.angle;
-        //         const a = b.vel.angle;
-        //         const s = b.vel.length * (1 + ufo.power * 4);
-        //         b.vel.set().addWithAngle(a - ra * 2, s);
-        //         b.pos.addWithAngle(a - ra * 2, s * 2);
-        //         if (b.pos.y < 20) {
-        //             b.pos.y = 20;
-        //         }
-        //     } else if (c.char.a || c.char.b) {
-        //         play("lucky");
-        //         end();
-        //     }
-        // }
-        if (c.char.a || c.char.b) {
-            play("lucky");
-            end();
-        }
-        // if (b.vel.y > 0 && (c.char.c || c.char.d || c.rect.light_black)) {
-        //     play("explosion");
-        //     const s = b.vel.length / sqrt(difficulty);
-        //     const radius = s * s;
-        //     const duration = sqrt(radius) * 9;
-        //     explosions.push({
-        //         pos: b.pos,
-        //         radius,
-        //         ticks: duration,
-        //         duration,
-        //     });
-        //     return true;
-        // }
-        return !b.pos.isInRect(-3, -3, 106, 106);
-    });
 
     // Draw player in the scene, give it animation and set collider for end condition
-    color("black");
+    color("cyan");
     const isCollidingToPlayer = char(addWithCharCode("a", floor(ticks / 10) % 2), player.pos.x, player.pos.y, {
         mirror: { x: player.vx > 0 ? 1 : -1 },
     }).isColliding
 
+    // Bullet Conditions
+    remove(bullets, (b) => {
+        b.pos.add(b.vel);
+        color(b.vel.y < 0 ? "red" : "purple");
+        const c = bar(b.pos, 4, 3, b.vel.angle).isColliding;
+        if (b.vel.y > 0) {
+            if (c.rect.black) {
+                play("powerUp");
+                b.vel.y = -b.vel.y;
+                b.vel.x = -b.vel.x + rnd(-2, 2);
+                b.isDeflected = true;
+            } else if (c.char.a || c.char.b) {
+                play("lucky");
+                end();
+            }
+        }
+        if (b.isDeflected) {
+            if (b.vel.y < 0 && (c.rect.yellow)) {
+                const radius = 10 + difficulty;
+                const duration = sqrt(radius) * 9;
+                explosions.push({
+                    pos: b.pos,
+                    radius,
+                    ticks: duration,
+                    duration,
+                });
+                return true;
+            }
+        }
+        return !b.pos.isInRect(-3, -3, 106, 106);
+    });
+
+    // End condition when player is collide with bullets
     if (isCollidingToPlayer.char.c || isCollidingToPlayer.char.d) {
-        // play("lucky");
-        // end();
+        play("lucky");
+        end();
     }
 }
